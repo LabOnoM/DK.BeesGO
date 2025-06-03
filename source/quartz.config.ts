fdca31318aeb4a55cdc9041b0bdcc62fcf422dc3
wong-ziyi <w751747834@gmail.com>
2025-06-03 16:52:18 +0900

    update homepage url

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://www.bs-gou.com/DK.BeesGO/",
+    baseUrl: "https://www.bs-gou.com/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -25,7 +25,7 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      "BSGOU": "https://www.bs-gou.com/",
+      "BSGOU": "https://www.bs-gou.com/blog/",
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },

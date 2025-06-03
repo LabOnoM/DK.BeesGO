6cd8e350bd1ebb459cb98a7330dc11a24bc8fad8
Wang Ziyi <9590738+wong-ziyi@users.noreply.github.com>
2025-06-03 15:25:54 +0900

    Create Utterances.ts

---
+++ source/quartz/components/Utterances.ts
@@ -0,0 +1,15 @@
+// quartz/components/Utterances.ts
+
+export function Utterances() {
+  return `
+    <section id="comments" style="margin-top:3em;">
+      <script src="https://utteranc.es/client.js"
+        repo="yourusername/your-repo"
+        issue-term="pathname"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+      </script>
+    </section>
+  `
+}
